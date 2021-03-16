c2aa33f89e5a3b3154af160ff5cb894522639a19
Paul Rogov <1624921+pvrogov@users.noreply.github.com>
2021-03-16 10:19:17 +0300

    Update single-quote-number-test.sh

--- single-quote-number-test.sh
+++ single-quote-number-test.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 while :
 do
-  read -p "Enter a number: " n
-  [ $n == 0 ] && echo "x is 0"
-  [ $n == 10 ] && echo "x is 10"
-  [ $n != 8 ] && echo "x is not 8"
+  read -p "Enter a number: " x
+  [ $x == 0 ] && echo "x is 0" # better use this: [ $x -eq 0 ] 
+  [ $x == 10 ] && echo "x is 10" # better use this: [ $x -eq 10 ]
+  [ $x != 8 ] && echo "x is not 8" # better use this: [ $x -ne 8 ]
+  [ $x -gt 5 ] && echo "x > 5"
+  [ $x -gt 0 ] && echo "x is positive"
+  [ $x -lt 0 ] && echo "x is negative"
+  echo
 done
