931497ff698b2c4237ed5e28fb463b339790f218
Paul Rogov <1624921+pvrogov@users.noreply.github.com>
2021-03-16 11:44:15 +0300

    Update string-comparison.sh

--- string-comparison.sh
+++ string-comparison.sh
@@ -7,9 +7,14 @@ do
   [ "$s" == "hey boss" ] && echo "Good! hey boss"
   [ "$s" != "storm" ] && echo "\"$s\" is not \"storm\""
   [ -z "$s" ] && echo "The string is empty"
+  [ -n "$s" ] && echo "The string is not empty"
+  [ "$s" == "" ] && echo "The string is empty"
   if [ "$s" == "hello world" ]; then echo "Yes! hello world"; fi
   # double quotes == != > < =~
   [[ "$s" =~ img.*\.png ]] && echo "Pattern match: img*.png"
   [[ "$s" > "corner" ]] && echo "$s is greater than \"crown\"" || echo "$s is less than \"crown\""
+  [[ -z "$s" ]] && echo "[[ string is empty"
+  [[ -n "$s" ]] && echo "[[ string is not empty"
+  [[ "$s" == "" ]] && echo "[[ string is empty"
   echo
 done
